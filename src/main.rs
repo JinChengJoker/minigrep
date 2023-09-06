2760894d94577e2652762d1c956a0bb9b1df805b
Jin Cheng <jinchengjoker@foxmail.com>
2023-09-06 17:59:38 +0800

    fn parse_config

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("query is {}", query);
     println!("file_path is {}", file_path);
@@ -12,4 +11,11 @@ fn main() {
     let content = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("file content:\n{content}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
