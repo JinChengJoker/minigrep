f81d901d476baf3acb06c76e7a0f0f92f922885d
Jin Cheng <jinchengjoker@foxmail.com>
2023-09-11 15:08:00 +0800

    add fn search

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,11 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    fs::read_to_string(config.file_path)?;
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+      println!("{line}");
+    };
 
     Ok(())
 }
@@ -32,14 +36,22 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-            Rust:
-            safe, fast, productive.
-            Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  vec![]
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+      if line.contains(query) {
+        results.push(line);
+      };
+  }
+
+  results
 }
