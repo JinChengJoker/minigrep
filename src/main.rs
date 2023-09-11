c091befa01f0f7062b3d7a0073b571697b1e74ff
Jin Cheng <jinchengjoker@foxmail.com>
2023-09-11 10:45:51 +0800

    impl Config

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("query is {}", config.query);
     println!("file_path is {}", config.file_path);
 
-    let content = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    let content =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("file content:\n{content}");
 }
@@ -18,9 +19,11 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Config { query, file_path }
+    }
 }
